7b174a56599c76a5fd1bcda2ffef2a197bcc9d0b
gjf7 <501018786@qq.com>
2025-05-11 11:23:15 +0800

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -333,12 +333,13 @@ if not vim.g.vscode then
     },
   })
 
-  local lspconfig = require("lspconfig")
-  lspconfig.eslint.setup({
-    on_attach = function(_, bufnr)
+  local base_on_attach = vim.lsp.config.eslint.on_attach
+  vim.lsp.config('eslint', {
+    on_attach = function(client, bufnr)
+      base_on_attach(client)
       vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = bufnr,
-        command = "EslintFixAll",
+        command = "LspEslintFixAll",
       })
     end,
   })
