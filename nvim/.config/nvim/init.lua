1bc6c6f0842363b4f9458f6ddb0ebf641fadb94f
gjf7 <hey@haochen.me>
2025-05-31 16:23:55 +0800

    disable plugin updates notification

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -84,11 +84,11 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out =
-      vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
 
       { "\nPress any key to exit..." },
     }, true, {})
@@ -423,7 +423,7 @@ require("lazy").setup({
       opts = {},
     },
   },
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 vim.api.nvim_create_user_command("FormatDisable", function(args)
